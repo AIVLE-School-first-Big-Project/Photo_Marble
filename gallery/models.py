b8c4b4c8771b3bfa5dd07f726eebb36cec553251
olsunghyun0416 <62636399+olsunghyun0416@users.noreply.github.com>
2022-04-20 16:32:08 +0900

    갤러리 기능 추가

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Gallery(models.Model):
+    galleryId = models.IntegerField(primary_key=True)
+    categoryId = models.IntegerField()
+    photo = models.TextField()
+    userId = models.IntegerField()
+    updateAt = models.DateTimeField()
+    createdAt = models.DateTimeField()
+
+class Like(models.Model):
+    like_id = models.IntegerField(primary_key=True)
+    gallery_id = models.IntegerField()
+    user_id = models.IntegerField()
+
+class Comment(models.Model):
+    comment_id = models.IntegerField(primary_key=True)
+    content = models.TextField()
+    updateAt = models.DateTimeField()
+    gallery_id = models.IntegerField()
+    user_id = models.IntegerField()
