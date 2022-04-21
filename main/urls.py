6cb60271d0e155063d8bd0787fbb35ae843bc996
easta <1210eastar@gmail.com>
2022-04-21 11:04:03 +0900

    url 정리

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
     path('login/', views.login, name='login'),
     path('mypage/', views.mypage, name='mypage'),
-    path('main/', views.main, name='main'),#경주
+    path('', views.main, name='main'),#경주
 
 ]
