3a63a341f33932e038ef4dce26f169aa477e36f9
gjuu <hkjcom96@naver.com>
2022-04-21 15:37:51 +0900

    네비바 변경

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('mypage/', views.mypage, name='mypage'),
     path('', views.main, name='main'),#경주
+    path('main/', views.main, name='main'),
     # path('signup/', views.SignupView, name="account_signup"),
     path('signup2/', views.CustomSignupView.as_view(), name="custom_signup"),
+    path('gallery/', views.gallery, name='main'),
 ]
