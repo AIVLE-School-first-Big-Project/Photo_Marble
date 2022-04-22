2e47ef08cde85a88ec28cb8e17f698198d74ee0f
gjuu <hkjcom96@naver.com>
2022-04-22 11:00:53 +0900

    navbar 로그인 여부,login 후 메인페이지 연동,logout후 login페이지 연동

--- main/urls.py
+++ main/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('mypage/delete/', views.delete, name='delete'),
     path('', views.main, name='main'),
     path('main/', views.main, name='main'),
-    # path('signup/', views.SignupView, name="account_signup"),
-    path('signup/', views.CustomSignupView.as_view(), name="custom_signup"),
-  
-    path('gallery/', views.gallery, name='main'),
+    # path('signup/', views.SignupView, name="account_signup"),   
+    path('signup/', views.CustomSignupView.as_view(), name="custom_signup"),     
+    path('gallery/', views.gallery, name='gallery'),    
+    path('logout/', views.CustomSLogoutView.as_view(), name="custom_logout"),
 ]
