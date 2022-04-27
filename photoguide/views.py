2e0fac871d82582339e3f5065fb2e4ae55de8f7f
yoonjoong <yoonjoong311@gmail.com>
2022-04-27 18:57:25 +0900

    테스트

--- photoguide/views.py
+++ photoguide/views.py
@@ -3,11 +3,6 @@ from django.urls import reverse
 from main.models import User, Collection, Landmark, Locations, Gallery
 from .DLmodel.similarity_code import FeatureExtractor
 
-import os
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
-from tensorflow.keras.models import Model
-from keras.models import load_model
 import numpy as np
 import pandas as pd
 from PIL import Image
@@ -20,12 +15,10 @@ def photoguide(request):
 def photoguide_update(request):
     img = request.FILES['file']
     
-    cwd = os.getcwd()
 
     features = np.load('./photoguide/DLmodel/similartiy_features.npy')
     img_paths = pd.read_csv("./photoguide/DLmodel/img_paths.csv", index_col=0)
     img_paths = list(img_paths['0'])
-
 
 
 
